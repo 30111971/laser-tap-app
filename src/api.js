6d295694b7b7b10c825227e717287a2588a0b309
30111971 <matheusmaica@gmail.com>
2020-09-01 21:43:27 -0300

    first commit

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import socketIOClient from 'socket.io-client';
 
-let host = "http://localhost:3030/";
+let host = "http://localhost:3030/"
 
 const api  = axios.create({
   baseURL: host,
